83ed2ff699fb99df8e73a1043c1ba39c493669ff
Juan Pablo Castaño A <juan.castano.arango@correounivalle.edu.co>
2023-08-25 21:05:38 -0500

    Se realizan pruebas en Ejercicio listas, con un exito del 28%

--- app/src/main/scala/introduccion/EjercicioListas.scala
+++ app/src/main/scala/introduccion/EjercicioListas.scala
@@ -14,6 +14,17 @@ class EjercicioListas {
   def repetirListas(lista: List[Int], n: Int): List[List[Int]] = {
     var listaRepetida : List[List[Int]] = List()
     //Complete el código
+    require ( n >= 0, "Solo numeros positivos, no se admiten numeros negativos")
+    //Para representar la lista se usa "lista" (valga la rebundancia)
+    //También haré uso de .map el cual es un método de las colecciones de Scala el
+    //cual se utiliza para transformar cada elemento de una colección aplicando
+    // una función a cada uno de ellos.
+    //List.fill(n)(element) Esta es la función que se aplica a cada elemento de la lista
+    //donde "element) representa a los elementos orignales de la lista que en este caso
+    //seria (List(1, 2, 3,),2)
+
+    lista.map(element => List.fill(n)(element))
+
     throw new UnsupportedOperationException("No implementado aún")
   }
   /*
